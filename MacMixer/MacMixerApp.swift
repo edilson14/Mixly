824e9fbf90c16cb46ce5aa43851904c8d9cdf565
edilson.tavares <edytavares66@gmail.com>
2026-07-23 20:36:20 -0300

    Initial Commit

---
+++ MacMixer/MacMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMixerApp.swift
+//  MacMixer
+//
+//  Created by edilson14 on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MacMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
